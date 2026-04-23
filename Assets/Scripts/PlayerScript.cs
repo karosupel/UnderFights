8609aad6ecf7be46920b60a99342e4473f44c249
karosupel <karosupel@gmail.com>
2026-04-23 14:10:43 +0200

    Player Health Bar added
    -> ui canvas
    -> player slider
    -> idamageable logic interface
    -> player can take damage under space (for testing reasons)

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,24 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerScript : MonoBehaviour
-{
+public class PlayerScript : MonoBehaviour, IDamageable
+{   
+    [Header("Movement")]
     [SerializeField] float speed;
     [SerializeField] Rigidbody2D rb;
+
+    [Header("Health")]
+    [SerializeField] float maxHealth;
+    //to delete this later v
+    public float health;
+    public Slider slider;
 
 
     void Start()
     {
-        
+        health = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = health;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
     }
 
+    //movement:
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -26,5 +39,11 @@ public class PlayerScript : MonoBehaviour
 
         Vector2 move = new Vector2(horizontal, vertical).normalized;
         rb.velocity = move * speed;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        slider.value = health;
     }
 }
